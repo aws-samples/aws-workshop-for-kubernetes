0361e5e071d15f06aad974897c0eaa6c9ff4feaa
Arun Gupta <arun.gupta@gmail.com>
2017-10-30 06:30:39 -0700

    starting work on https://github.com/arun-gupta/kubernetes-aws-workshop/issues/126

---
+++ app-upgrade/images/app-v1/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
